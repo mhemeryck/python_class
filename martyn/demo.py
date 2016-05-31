0a3825dbf7c419198391158e03472a47e1f3eecb
Martijn Hemeryck <martijn.hemeryck@soundtalks.com>
2016-05-31 09:42:46 +0200

    demo: make a recording object

--- martyn/demo.py
+++ martyn/demo.py
@@ -1,3 +1,13 @@
+from scipy.io import wavfile
+
 import labelbox
+from labelbox.models import Recording
+
+AUDIO_FILE = '../test.wav'
 
 print labelbox.__name__, labelbox.__author__, labelbox.__version__
+
+fs, sound = wavfile.read(AUDIO_FILE)
+recording = Recording(filename=AUDIO_FILE, fs=fs)
+
+print recording.filename, recording.fs
